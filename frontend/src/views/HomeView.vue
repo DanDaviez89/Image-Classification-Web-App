824972e473f3f3bfaea7de5277c0b966089aa70c
Dan Davies <dandavies89@yahoo.co.uk>
2023-12-02 11:04:59 +1000

    installing axios, making request to backend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+
+    <button @click="test">Click me</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+      async test() {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/test/');
+
+            console.log(response.data.message)
+        } catch (error) {
+          console.log(error)
+        }
+
+        this.userInput = ''
+      }
+    }
+}
 </script>
 
 <style>
