261288865116c3ea3638b9f86697ab0cf4801e50
Dan Davies <dandavies89@yahoo.co.uk>
2023-12-02 10:47:02 +1000

    Setting up vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .home {
+    width: 80%;
+    margin: 5rem auto;
+  }
+</style>
